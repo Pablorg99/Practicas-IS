c206108ad3d9a41b1705b356cfd53b2baffe54ed
Pablorg99 <i72rogup@uco.es>
2018-12-03 12:31:48 +0100

    Identación con 4 espacios y operadores para E/S ficheros txt añadidos

--- 4_Implementacion/Codigo/alumno.cc
+++ 4_Implementacion/Codigo/alumno.cc
@@ -1,35 +1,63 @@
 #include "alumno.h"
 
 Alumno::Alumno(string dni, string nombre, string apellidos,
-  int telefono, string direccion, string email,
-  int curso, int Nequipo, bool lider): Persona(dni, nombre, 
-  apellidos, telefono, direccion, email){
-    setCurso(curso);
-    setNequipo(Nequipo);
-    //La función setLider necesita corregirse, aquí se pasa un booleano y la función recibe un objeto Alumno
-    setLider(lider);
-  }
-  
-void Alumno::setLider(Alumno aux){
-  if(aux.getLider()){ lider_ = false; }
-  else{ lider_ = true; }
+	int telefono, string direccion, string email,
+	int curso, int Nequipo, bool lider): Persona(dni, nombre, 
+	apellidos, telefono, direccion, email){
+	setCurso(curso);
+	setNequipo(Nequipo);
+	//La función setLider necesita corregirse, aquí se pasa un booleano y la función recibe un objeto Alumno
+	lider_ = lider;
 }
 
+void Alumno::cambiaLider(){
+	//Falta comprobar nº lideres en equipo = 1
+	if(getLider()) lider_ = false;
+	else lider_ = true;
+}
+
+void Alumno::setLider(bool nuevo_lider) {
+	//Falta comprobar nº lideres en equipo = 1
+	lider_ = nuevo_lider;
+}
 
 ostream &operator<<(ostream &output_stream, const Alumno &alumno) {
-  //IN PROGRESS
-  size_t string_size;
-  string_size = alumno.getDNI().size();
-  output_stream.write(alumno.getDNI().data(), string_size);
-  string_size = alumno.getNombre().size();
-  output_stream.write(alumno.getNombre().data(), string_size);
-  string_size = alumno.getApellidos().size();
-  output_stream.write(alumno.getApellidos().data(), string_size);
-
-  string_size = alumno.getDireccion().size();
-  output_stream.write(alumno.getDireccion().data(), string_size);
-
+	string bool_lider;
+	if(alumno.getLider()) bool_lider = "true";
+	else bool_lider = "false";
+	
+	output_stream << alumno.getDNI() << ',' << alumno.getNombre() << 
+		alumno.getApellidos() << ',' << alumno.getTelefono() << ','
+		<< alumno.getDireccion() << ',' << alumno.getEmail() << ','
+		<< alumno.getCurso() << ',' << alumno.getNequipo() << ','
+		<< bool_lider << '\n';
+	
+	return output_stream;
 }
+
 istream &operator>>(istream &input_stream, Alumno &alumno) {
-  //IN PROGRESS  
+	string aux_string;
+	bool is_lider;
+	getline(input_stream, aux_string, ',');
+	alumno.setDNI(aux_string);
+	getline(input_stream, aux_string, ',');
+	alumno.setNombre(aux_string);
+	getline(input_stream, aux_string, ',');
+	alumno.setApellido(aux_string);
+	getline(input_stream, aux_string, ',');
+	alumno.setTelefono(stoi(aux_string));
+	getline(input_stream, aux_string, ',');
+	alumno.setDireccion(aux_string);
+	getline(input_stream, aux_string, ',');
+	alumno.setEmail(aux_string);
+	getline(input_stream, aux_string, ',');
+	alumno.setCurso(stoi(aux_string));
+	getline(input_stream, aux_string, ',');
+	alumno.setNequipo(stoi(aux_string));
+	getline(input_stream, aux_string, '\n');
+	if(aux_string == "true") is_lider = true;
+	else is_lider = false;
+	alumno.setLider(is_lider);
+	
+	return input_stream;
 }
--- 4_Implementacion/Codigo/alumno.h
+++ 4_Implementacion/Codigo/alumno.h
@@ -10,30 +10,30 @@ using std::ostream;
 using std::istream;
 
 class Alumno :public Persona{
-  private:
-    int curso_;
-    int Nequipo_;
-    bool lider_;
+	private:
+		int curso_;
+		int Nequipo_;
+		bool lider_;
 
-  public:
-    //Constructor
-    Alumno(string dni, string nombre, string apellidos,
-    int telefono=0, string direccion="", string email="",
-    int curso=0, int Nequipo=0, bool lider=false);
+	public:
+		//Constructor
+		Alumno(string dni, string nombre, string apellidos,
+		int telefono=0, string direccion="", string email="",
+		int curso=0, int Nequipo=0, bool lider=false);
 
-    //Modificadores
-    inline void setCurso(int nuevo_curso){curso_ = nuevo_curso;};
-    inline void setNequipo(int nuevo_Nequipo){Nequipo_=nuevo_Nequipo;};
-    void setLider(Alumno aux);           //Se encarga de cambiar el valor de lider_ entre true y false
+		//Modificadores
+		inline void setCurso(int nuevo_curso){curso_ = nuevo_curso;};
+		inline void setNequipo(int nuevo_Nequipo){Nequipo_=nuevo_Nequipo;};
+		void setLider(bool nuevo_lider);
+		void cambiaLider();           //Se encarga de cambiar el valor de lider_ entre true y false
+		//Observadores
+		inline int getCurso() const {return curso_;};
+		inline int getNequipo() const {return Nequipo_;};
+		inline bool getLider() const {return lider_;};
 
-    //Observadores
-    inline int getCurso() const {return curso_;};
-    inline int getNequipo() const {return Nequipo_;};
-    inline bool getLider() const {return lider_;};
-
-    //Operadores
-    friend ostream &operator<<(ostream &output_stream, const Alumno &alumno);
-    friend istream &operator>>(istream &input_stream, Alumno &alumno);
+		//Operadores
+		friend ostream &operator<<(ostream &output_stream, const Alumno &alumno);
+		friend istream &operator>>(istream &input_stream, Alumno &alumno);
 };
 
 #endif
