d311286eff7b3decd7607a2d05cef622077a94bc
Pablorg99 <i72rogup@uco.es>
2018-12-16 13:09:13 +0100

    Operadores E/S de Profesor añadidos

--- 4_Implementacion/Codigo/profesor.cc
+++ 4_Implementacion/Codigo/profesor.cc
@@ -1,7 +1,7 @@
 #include "profesor.h"
+#include <fstream>
 
-
-bool Profesor::setCoordinador(){
+bool Profesor::CambiarCoordinador(){
     if(coordinador_){
         coordinador_ = false;
     }
@@ -9,4 +9,42 @@ bool Profesor::setCoordinador(){
         coordinador_ = true;
     }
     return coordinador_;
+}
+
+ostream &operator<<(ostream &output_stream, const Profesor &profesor) {
+	string bool_coordinador;
+	if(profesor.getCoordinador()) bool_coordinador = "true";
+	else bool_coordinador = "false";
+
+	output_stream << profesor.getDNI() << ',' << profesor.getNombre() <<
+		profesor.getFichero() << ',' << profesor.getApellidos() << ',' 
+		<< profesor.getTelefono() << ',' << profesor.getDireccion() 
+		<< ',' << profesor.getEmail() << ',' << bool_coordinador << '\n';
+
+	return output_stream;
+}
+
+istream &operator>>(istream &input_stream, Profesor &profesor) {
+	string aux_string;
+	bool is_coordinador;
+	getline(input_stream, aux_string, ',');
+	profesor.setDNI(aux_string);
+	getline(input_stream, aux_string, ',');
+	profesor.setNombre(aux_string);
+	getline(input_stream, aux_string, ',');
+	profesor.setFichero(aux_string);
+	getline(input_stream, aux_string, ',');
+	profesor.setApellido(aux_string);
+	getline(input_stream, aux_string, ',');
+	profesor.setTelefono(stoi(aux_string));
+	getline(input_stream, aux_string, ',');
+	profesor.setDireccion(aux_string);
+	getline(input_stream, aux_string, ',');
+	profesor.setEmail(aux_string);
+	getline(input_stream, aux_string, '\n');
+	if(aux_string == "true") is_coordinador = true;
+	else is_coordinador = false;
+	profesor.setCoordinador(is_coordinador);
+
+	return input_stream;
 }
